10fa9022766f58ced9459374df1a3e409406b703
kimsj018 <kimsj0118@gmail.com>
2025-08-27 16:45:37 +0900

    Day17 필수과제

---
+++ board/day17-my-app/src/routes/boards/new/BoardsDetail.js
@@ -0,0 +1,97 @@
+import { Link } from "react-router"
+import './BoardsDetail.css'
+
+const BoardsDetail = () => {
+
+    return (
+
+        <div className="main">
+            <h1>살어리 살어리랏다 쳥산(靑山)애 살어리랏다멀위랑 ᄃᆞ래랑 먹고 쳥산(靑山)애 살어리랏다얄리얄리 얄랑셩 얄라리 얄라</h1>
+            <div id="section1">
+                <div id="section1__1">
+                    <div id="section1__1__1">
+                        <div id="section1__1__1__1">
+                            <img id="icon1" src="./assets/icons/profile.png"></img>
+                        </div>
+                        <spna>홍길동</spna>
+                    </div>
+                    <div>
+                        <span>2024.11.11</span>
+                    </div>
+                </div>
+                <hr />
+                <div id="section1__3">
+                    <img id="icon2" src="./assets/icons/link.png" />
+                    <img id="icon3" src="./assets/icons/jido.png" />
+                </div>
+            </div>
+
+            <img id="img1" src="./assets/images/BeachsideSerenity 1.png" />
+
+            <div id="text">
+                살겠노라 살겠노라. 청산에 살겠노라. <br />
+                머루랑 다래를 먹고 청산에 살겠노라.<br />
+                얄리얄리 얄랑셩 얄라리 얄라<br />
+                <br />
+                우는구나 우는구나 새야. 자고 일어나 우는구나 새야.<br />
+                너보다 시름 많은 나도 자고 일어나 우노라.<br />
+                얄리얄리 얄라셩 얄라리 얄라<br />
+                <br />
+                갈던 밭(사래) 갈던 밭 보았느냐. 물 아래(근처) 갈던 밭 보았느냐<br />
+                이끼 묻은 쟁기를 가지고 물 아래 갈던 밭 보았느냐.<br />
+                얄리얄리 얄라셩 얄라리 얄라<br />
+                <br />
+                이럭저럭 하여 낮일랑 지내 왔건만<br />
+                올 이도 갈 이도 없는 밤일랑 또 어찌 할 것인가.<br />
+                얄리얄리 얄라셩 얄라리 얄라<br />
+                <br />
+                어디다 던지는 돌인가 누구를 맞히려던 돌인가.<br />
+                미워할 이도 사랑할 이도 없이 맞아서 우노라.<br />
+                얄리얄리 얄라셩 얄라리 얄라<br />
+                <br />
+                살겠노라 살겠노라. 바다에 살겠노라.<br />
+                나문재, 굴, 조개를 먹고 바다에 살겠노라.<br />
+                얄리얄리 얄라셩 얄라리 얄라<br />
+                <br />
+                가다가 가다가 듣노라. 에정지(미상) 가다가 듣노라.<br />
+                사슴(탈 쓴 광대)이 솟대에 올라서 해금을 켜는 것을 듣노라.<br />
+                얄리얄리 얄라셩 얄라리 얄라<br />
+                <br />
+                가다 보니 배불룩한 술독에 독한 술을 빚는구나.<br />
+                조롱박꽃 모양 누룩이 매워 (나를) 붙잡으니 내 어찌 하리이까.[1]<br />
+                얄리얄리 얄라셩 얄라리 얄라
+            </div>
+
+            <div id="img2__back">
+                <img id="img2" src="./assets/images/Frame4.png"></img>
+            </div>
+
+            <div id="section2">
+                <div className="section2__1">
+                    <img src="./assets/icons/bad.png"></img>
+                    <div>24</div>
+                </div>
+                <div className="section2__1">
+                    <img src="./assets/icons/good.png"></img>
+                    <div>12</div>
+                </div>
+            </div>
+
+            <div className="section3">
+                <div className="section3__1">
+                    <img id="icon4" src="./assets/icons/vector.png"></img>
+                    <div>목록으로</div>
+                </div>
+                <div className="section3__1">
+                    <img id="icon5" src="./assets/icons/left_icon.png"></img>
+                    <div>수정하기</div>
+                </div>
+
+            </div>
+
+
+        </div>
+    )
+}
+
+export default BoardsDetail
---
+++ board/day17-my-app/src/routes/boards/new/boardsNew.js
@@ -0,0 +1,187 @@
+import { Link } from "react-router"
+import { useState } from 'react'
+import '../../../App.css';
+
+const BoardNew = () => {
+
+    
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [title, setTitle] = useState("")
+  const [content, setContent] = useState("")
+
+
+  const [emailError, setEmailError] = useState("")
+  const [passwordError, setPasswordError] = useState("")
+  const [titleError, setTitleError] = useState("")
+  const [contentError, setContentError] = useState("")
+  const [isActive, setIsActive] = useState(false)
+
+
+  const onChangeEmail = (event) => {
+    setEmail(event.target.value)
+
+    if(event.target.value){
+        setEmailError("")
+    }else{
+        setEmailError("필수입력사항")
+    }
+
+    if(event.target.value && password && title && content){
+        setIsActive(true)
+    }else{
+        setIsActive(false)
+    }
+  }
+
+  const onChangePassword = (event) => {
+    setPassword(event.target.value)
+
+    if(event.target.value){
+        setPasswordError("")
+    }else{
+        setPasswordError("필수입력사항")
+    }
+
+    if(email && event.target.value && title && content){
+        
+        setIsActive(true)
+
+    }else{
+        
+        setIsActive(false)
+    }
+
+  }
+
+  const onChangeTitle = (event) => {
+    setTitle(event.target.value)
+
+    if(event.target.value){
+        setTitleError("")
+    }else{
+        setTitleError("필수입력사항")
+    }
+    
+
+    if(email && password && event.target.value && content){
+        setIsActive(true)
+    }else{
+        setIsActive(false)
+    }
+  }
+
+  const onChangeContent = (event) => {
+    setContent(event.target.value)
+
+    if(event.target.value){
+        setContentError("")
+    }else{
+        setContentError("필수입력사항")
+    }
+
+    if(email && password && title && event.target.value){
+        setIsActive(true)
+    }else{
+        setIsActive(false)
+    }
+  }
+
+  const onClickSignUp = () => {
+    
+    if (email && password  && title && content) {
+      alert("게시글 등록이 가능한 상태입니다.")
+    }
+  }
+
+
+
+
+
+
+
+  return (
+    <>
+      <div id="head">게시물 등록</div>
+      <div id="main">
+        <div className="메인">
+          <div className="메인__1__1">
+            <div>작성자 <span className="필수">*</span> </div>
+            <input type="text" placeholder="작성자 명을 입력해 주세요." className="메인1__인풋" onChange={onChangeEmail}></input>
+            <div className='errorMassage'>{emailError}</div>
+          </div>
+          <div className="메인__1__1">
+            <div>비밀번호 <span className="필수">*</span></div>
+            <input type="password" placeholder="비밀번호를 입력해 주세요." className="메인1__인풋" onChange={onChangePassword}></input>
+            <div className='errorMassage'>{passwordError}</div>
+          </div>
+        </div>
+        <hr></hr>
+        <div id="메인__제목" className="메인__1__1">
+          <div>제목 <span className="필수">*</span></div>
+          <input type="text" placeholder="제목을 입력해 주세요." onChange={onChangeTitle}></input>
+          <div className='errorMassage'>{titleError}</div>
+        </div>
+        <hr></hr>
+
+        <div id="메인__내용" className="메인__1__1">
+          <div>내용 <span className="필수">*</span></div>
+          <textarea placeholder="내용을 입력해 주세요." onChange={onChangeContent}></textarea>
+          <div className='errorMassage'>{contentError}</div>
+        </div>
+
+
+        <div id="메인__주소" className="메인__1__1">
+          <div className="메인__1__1" id="메인__주소__검색">
+            <div>주소</div>
+            <div id="메인__주소__주소검색">
+              <input type="number" placeholder="01234" id="주소검색인풋"></input>
+              <button id="주소검색버튼">우편번호 검색</button>
+            </div>
+          </div>
+          <div>
+            <input type="text" placeholder="주소를 입력해 주세요."></input>
+          </div>
+          <div>
+            <input type="text" placeholder="상세주소"></input>
+          </div>
+        </div>
+        <hr></hr>
+
+        <div id="메인__유튜브링크" className="메인__1__1">
+          <div>유튜브 링크</div>
+          <input type="text" placeholder="링크를 입력해주세요."></input>
+        </div>
+        <hr></hr>
+
+        <div className="메인__1__1">
+          <div>사진 첨부</div>
+          <div id="사진첨부목록">
+            <div className="사진첨부리스트">
+              <img src="../assets/icons/add.png"></img>
+              <div>클릭해서 사진 업로드</div>
+            </div>
+            <div className="사진첨부리스트">
+              <img src="../assets/icons/add.png"></img>
+              <div>클릭해서 사진 업로드</div>
+            </div>
+            <div className="사진첨부리스트">
+              <img src="../assets/icons/add.png"></img>
+              <div>클릭해서 사진 업로드</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="footer">
+        <button id="푸터취소버튼">취소</button>
+        <button id="푸터등록버튼" onClick={onClickSignUp} disabled={!isActive} style={{ backgroundColor: isActive ? "blue" : "gray" }}>등록하기</button>
+
+      </div>
+
+    </>
+
+  );
+    
+}
+
+export default BoardNew
